5b5451678510eb34a8fabb72f2765f2c598926a5
Gaël Philippe <gaelph@gmail.com>
2024-05-01 15:03:50 +0200

    fix(package): rewrote the Package.swift to be 5.9 compatible

--- Package.swift
+++ Package.swift
@@ -1,6 +1,32 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "DiffMatchPatch",
-    targets: [Target(name: "DiffMatchPatch", dependencies:["diff_match_patch"])]
+	platforms: [
+		.macOS(.v10_15)
+	],
+	products: [
+		.library(
+			name: "diff_match_patch",
+			type: .static,
+			targets: [
+				"DiffMatchPatch"
+			]
+		)
+	],
+	targets: [
+		.target(
+			name: "diff_match_patch"
+		),
+		.target(
+			name: "DiffMatchPatch",
+			dependencies: [
+				"diff_match_patch"
+			],
+			cSettings: [
+				.headerSearchPath("../diff_match_path/include")
+			]
+		)
+	]
 )
